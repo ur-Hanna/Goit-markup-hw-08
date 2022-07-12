48b608ac2f33615ee53c1650b5b6aa2e56836ebd
Hanna Rosytiuk <hanna.mainmail@gmail.com>
2022-07-12 18:24:44 +0300

    Добавляет no-scroll

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,10 @@
     menu: document.querySelector('.mobile-menu'),
     body: document.querySelector('body'),
   };
+
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
